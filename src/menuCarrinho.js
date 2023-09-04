5c0f0fd53865a9cd1e3fcbd592c9383a63dedf9f
Maria Eduarda Corrêa <133533208+me-maria@users.noreply.github.com>
2023-09-04 17:13:47 -0300

    add checkout e histórico de pedidos
    add formulário de pagamento(checkout)
    add histórico de pedido
    add estilização das pags

--- src/menuCarrinho.js
+++ src/menuCarrinho.js
@@ -14,6 +14,7 @@ function fecharCarrinho() {
 
 function irParaCheckout() {
   if (Object.keys(idsProdutoCarrinhoComQuantidade).length === 0) {
+    alert("Carrinho vazio")
     return;
   }
   window.location.href = "./checkout.html";
